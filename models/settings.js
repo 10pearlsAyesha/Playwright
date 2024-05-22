48dc8e014767c0545dce42816a4385700e97ed0f
Areeba Shuja <areeba.shuja@10pearls.com>
2024-05-22 15:29:54 +0500

    settings page automating

--- models/settings.js
+++ models/settings.js
@@ -8,13 +8,24 @@ class SettingsPage {
     this.languageDropdown = page.locator("//div[@class='v-select__selections']//div[@class='row']//div[1]//span");
     this.selectedLanguageInDropdown = page.locator("//div[@class='v-select__selections']//div[@class='row']//div[2]//span");
     this.selectAnotherLanguage = page.locator("//div[@class='row']//div[2]//span[contains(text(),'" + `${process.env.another_platform_language}` + "')]");
-    this.selectDefaultLanguage = page.locator("(//div[@class='v-list-item__content']//div[@class='row']//div[1]//span[contains(text(),'" + `${process.env.default_platform_language}` + "')])[2]");
-    this.saveButton = page.locator("//span[@class='v-btn__content'][contains(text(),'Save')]");
+    this.selectDefaultLanguage = page.locator("//div[@class='v-list-item__content']//div[@class='row']//div[1]//span[contains(text(),'" + `${process.env.default_platform_language}` + "')]");
+    this.platformLanguageSaveButton = page.locator("//button[contains(@class,'save')]//span");
 
     this.supportOption = page.locator("//div[@class='options-lists']//div[@class='content']//h2[contains(text(),'Support')]");
-    this.privacyOption = page.locator("//div[@class='options-lists']//div[@class='content']//h2[contains(text(),'Privacy')]");
-    this.changePasswordOption = page.locator("//div[@class='options-lists']//div[@class='content']//h2[contains(text(),'Change Password')]");
+    this.feedbackReasonDropdown = page.locator("//div[@class='v-select__selections']//div[@class='v-select__selection v-select__selection--comma']");
+    this.selectFeedbackReason = page.locator("//div[@class='v-list-item__title'][contains(text(),'" + `${process.env.feedback_reason}` + "')]");
+    this.addFeedbackMessage = page.locator("//textarea[@placeholder='Please enter your message.']");
+    this.supportSubmitButton = page.locator("//span[@class='v-btn__content'][contains(text(),'Submit')]");
+    this.supportThankyouText = page.locator("//h2[@class='tagline text-center'][contains(text(),'" + `${process.env.support_thankyou_text}` + "')]");
 
+    this.privacyOption = page.locator("//div[@class='options-lists']//div[@class='content']//h2[contains(text(),'Privacy')]");
+    this.termsOfUseOption = page.locator("//a[@id='terms-of-use']");
+    this.privacyNoticeOption = page.locator("//a[@id='privacy-notice']");
+    this.deleteAccountOption = page.locator("//div[@class='content']//h2[contains(text(),'Delete Account')]");
+
+
+
+    this.changePasswordOption = page.locator("//div[@class='options-lists']//div[@class='content']//h2[contains(text(),'Change Password')]");
     // this. = page.locator("");
     // this. = page.locator("");
     // this. = page.locator("");
@@ -22,30 +33,45 @@ class SettingsPage {
     this.logoutButton = page.locator("//span[@class='v-btn__content'][contains(text(),'Log out')]");    
   }
 
-  //Actions  
+  //Platform Language  
   async platformLanguage() {
-    await this.settingsTab.click();
     await this.platformLanguageOption.click();
     await this.languageDropdown.click();
     await this.selectAnotherLanguage.click();
-    await this.saveButton.click();
+    await this.platformLanguageSaveButton.click();
     await this.platformLanguageOption.click();
 
     const selectedLanguage = await this.selectedLanguageInDropdown.textContent();
-    console.log("selected language: " + selectedLanguage);
     expect(selectedLanguage).toBe(`${process.env.another_platform_language}`);
 
     await this.languageDropdown.click();
     await this.selectDefaultLanguage.click();
-    await this.saveButton.click();
+    await this.platformLanguageSaveButton.click();
   }
 
+  //Support
   async support() {
+    await this.supportOption.click();
+    await this.feedbackReasonDropdown.click();
+    await this.selectFeedbackReason.click();
+    await this.addFeedbackMessage.type(`${process.env.feedback_message}`);
+    await this.supportSubmitButton.click();
+    await expect(this.supportThankyouText).toBeVisible(); 
   }
 
-  async privacy() {
+  //Privacy 
+  async privacyTermsOfUse() {
+    await this.privacyOption.click();
+    
+    const [newPage] = await Promise.all([
+      this.page.context().waitForEvent('page'),
+      this.termsOfUseOption.click()
+    ]);
+    await newPage.waitForLoadState();
+    return newPage;
   }
 
+  //Change Password
   async changePassword() {
   }
 }
--- tests/settings-page-tests.spec.js
+++ tests/settings-page-tests.spec.js
@@ -3,32 +3,54 @@ const LoginPage = require("../models/login");
 const SettingsPage = require("../models/settings");
 
 test.describe("User is on Settings Page", () => {
-//  let settingsPage;
 
   test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
+    const settingsPage = new SettingsPage(page);
 
     await page.goto(`${process.env.BASE_URL}`);
     await loginPage.loginCustomer();
+    await settingsPage.settingsTab.click();
   });
 
-  test.only("User changes platform language", async ({ page }) => {
+  test("User changes platform language", async ({ page }) => {
     const settingsPage = new SettingsPage(page);
 
     await settingsPage.platformLanguage();
   });
 
-  // test("", async () => {
-  // });
+  test("User priovides feedback through Support option", async ({ page }) => {
+    const settingsPage = new SettingsPage(page);
 
-  // test("", async () => {
-  // });
+    await settingsPage.support();
+  });
 
-  // test("", async () => {
-  // });
+  test.only("User sees privacy terms and policies", async ({ page }) => {
+    const settingsPage = new SettingsPage(page);
 
-  test("User logs out from Jeenie site", async () => {
-    await settingsPage.settingsTab.click();
+    const newPage = await settingsPage.privacyTermsOfUse();
+
+    await expect(newPage).toHaveURL(`${process.env.terms_of_use_link}`);
+
+    // const settingsPage = new SettingsPage(page);
+
+    // settingsPage.privacy();
+
+    // settingsPage.privacyOption.click();
+    
+    // const [newPage] = await Promise.all([
+    //   page.context().waitForEvent('page'),
+    //   await settingsPage.termsOfUseOption.click()
+    // ]);
+  
+    // await newPage.waitForLoadState();
+    // await expect(newPage).toHaveURL(`${process.env.terms_of_use_link}`);
+    // await expect(settingsPage.termsOfUseHeading).toBeVisible(); 
+  });
+
+  test("User logs out from Jeenie site", async ({ page }) => {
+    const settingsPage = new SettingsPage(page);
+
     await settingsPage.logoutButton.click();
   });
 });
